4dfcacf0138f48d9d86d62170d42cbcf9c808655
Sachin Gavali <98352918+sachin-gavali@users.noreply.github.com>
2022-12-04 04:40:18 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,29 @@
+// DAY -20 
+//PRACTICE OF ARRAY LAST DAY 
+// LINER SEARCH 
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = { 23,34,45,65,76};
+    int size = 5 ;
+    int target = 45;
+    int idx = 0 ;
+    // We can applay a linear Search for finding a target element in the array 
+    while(idx < size ){
+        if(arr[idx] == target){
+            cout << " Element is found on index : " << idx << endl;
+
+        }
+            idx = idx + 1 ;
+
+    }
+
+    // Insert element in the array 
+    int arr[] = { 23,34,45,65,76};
+    int size = 5 ;
+    int idx = 3 ;
+
+    for(int i = 0 ; i < size ; i++){
+        
+    }
+}
---
+++ Pointers.cpp
@@ -0,0 +1,45 @@
+// Pointers in c++
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = { 23,43,54,65,87};
+    int size  = 5 ;
+   /*  // valuse print using a pointers on array 
+    cout << " base value on Oth position :  " << *arr << endl;
+    cout <<  " Value on 0th position + 1  : " << *arr+1  << endl;
+    cout << " Value on 1st Position  : " << *(arr+1)<< endl;
+    cout << " Value on 3rd Position  : " << *(arr+2)<< endl;
+    cout << " Base address : " << arr <<  endl ;
+    // address can be incresing by 4 bit on each position  
+    cout << " Address on 1st Position :  " << &arr[1] << endl;
+    cout << " Address on 2nd Position :  " << &arr[2] << endl;
+    cout << " Address on 3rd Position :  " << &arr[3] << endl;
+    cout << " Address on 4th Position :  " << &arr[4] << endl;
+    cout << " Address on 5th Position :  " << &arr[5] << endl;
+
+    // Print the array element using for loop
+  cout << " Print array elements using a Pointer "<< endl;
+    for(int i = 0 ; i < size; i++){
+        cout << *(arr+i) <<endl;
+    } */
+ /*  cout << " Print array elements  "<< endl;
+    for(int i = 0 ; i < size; i++){
+        cout << i[arr] <<endl;
+    } */
+
+   /*  int a[10] = { 10,20,30,40};
+    int *ptr = &a[0];
+    cout << " Base Address : "<< endl; 
+    cout << a << endl;
+    cout << &a[0] << endl;
+    cout <<&a << endl;
+    cout << " Using Pointer " << endl;
+    cout << *ptr << endl;
+    cout << ptr << endl;
+    cout << &ptr << endl;
+    cout << *(ptr+1) << endl; */
+
+    
+
+    return 0 ;
+}
